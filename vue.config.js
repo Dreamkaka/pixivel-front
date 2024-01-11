36128675fcefe61941c8c05b9466f7c3141a34a3
HMXHKAKA <97349507+Dreamkaka@users.noreply.github.com>
2024-01-12 01:24:29 +0800

    change-logo

--- vue.config.js
+++ vue.config.js
@@ -9,12 +9,12 @@ module.exports = {
   },
   pwa: {
     name: "Pxelk",
-    themeColor: "#8F77B5",
+    themeColor: "#00ddff",
     msTileColor: "#000000",
     manifestOptions: {
-      name: "Pxelk",
-      short_name: "Pxelk",
-      theme_color: "#8F77B5",
+      name: "Pixvz",
+      short_name: "Pixvz",
+      theme_color: "#00ddff",
       background_color: "#fafafa",
       display: "standalone",
       scope: "./",
@@ -25,48 +25,9 @@ module.exports = {
           src: "img/icons/icon-48x48.png",
           sizes: "48x48",
           type: "image/png",
-        },
-        {
-          src: "img/icons/icon-72x72.png",
-          sizes: "72x72",
-          type: "image/png",
-        },
-        {
-          src: "img/icons/icon-96x96.png",
-          sizes: "96x96",
-          type: "image/png",
-        },
-        {
-          src: "img/icons/icon-128x128.png",
-          sizes: "128x128",
-          type: "image/png",
-        },
-        {
-          src: "img/icons/icon-144x144.png",
-          sizes: "144x144",
-          type: "image/png",
-        },
-        {
-          src: "img/icons/icon-152x152.png",
-          sizes: "152x152",
-          type: "image/png",
-        },
-        {
-          src: "img/icons/icon-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "img/icons/icon-256x256.png",
-          sizes: "256x256",
-          type: "image/png",
-        },
-        {
-          src: "img/icons/icon-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
+        }
+      ]
+
     },
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "#8F77B5",
